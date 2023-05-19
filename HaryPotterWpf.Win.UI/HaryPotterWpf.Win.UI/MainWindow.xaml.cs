779f824cccc51580aab639edb15f5edf3caf8f33
Evan BOISSONNOT <evan@boissonnot.fr>
2023-05-19 16:26:52 +0200

    feat: style and trigger

--- HaryPotterWpf.Win.UI/HaryPotterWpf.Win.UI/MainWindow.xaml.cs
+++ HaryPotterWpf.Win.UI/HaryPotterWpf.Win.UI/MainWindow.xaml.cs
@@ -25,6 +25,16 @@ namespace HaryPotterWpf.Win.UI
         private void btnName_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Test", "Test", MessageBoxButton.OK, MessageBoxImage.Error);
+            this.grdInputs.Visibility = Visibility.Hidden;
+            this.grdSteps.Visibility = Visibility.Visible;
+
+            //var resource = this.Resources["buttonColorBackground"] as Style;
+            //var setter = resource?.Setters.Where(item => ((Setter)item).Property.Name == "Background").First() as Setter;
+
+            //setter.Value = Colors.DarkGreen;
+
+           this.Resources["backgroundColor"] = new SolidColorBrush(Colors.DarkGreen);
+
         }
 
         private void StackPanel_Click(object sender, RoutedEventArgs e)
@@ -34,7 +44,17 @@ namespace HaryPotterWpf.Win.UI
 
         private void Grid_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
+            var textBox = e.Source as TextBox;
 
+            if (textBox != null && string.IsNullOrEmpty(textBox.Text))
+            {
+                MessageBox.Show("Un champs obligatoire est vide", "Requis", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
         }
     }
 }
