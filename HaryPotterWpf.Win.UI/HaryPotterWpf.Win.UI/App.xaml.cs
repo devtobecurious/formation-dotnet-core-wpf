b671993981c33f5dd34375e1d8d3095bd2951621
Evan BOISSONNOT <evan@boissonnot.fr>
2023-05-23 19:06:15 +0200

    feat: start of DI

--- HaryPotterWpf.Win.UI/HaryPotterWpf.Win.UI/App.xaml.cs
+++ HaryPotterWpf.Win.UI/HaryPotterWpf.Win.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,5 +11,30 @@ namespace HaryPotterWpf.Win.UI
     /// </summary>
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder().
+                      ConfigureServices((context, services) =>
+                      {
+                          services.AddSingleton<MainWindow>();
+                      }).Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var form = AppHost.Services.GetRequiredService<MainWindow>();
+            form.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+        }
     }
 }
