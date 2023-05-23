a4b84d77851e7aed2fcf531d044d413c3b7ea8b4
Evan BOISSONNOT <evan@boissonnot.fr>
2023-05-23 18:53:53 +0200

    feat: user controls

---
+++ HaryPotterWpf.Win.UI/HaryPotterWpf.Win.UI/Shared/MainMenu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HaryPotterWpf.Win.UI.Shared
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        public int NbItems { get; set; }
+    }
+}
